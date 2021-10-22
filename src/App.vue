d62c388a05f97a5fa9f239250bd88a6c86fd897f
Tommaso Maioli <tommaso.maioli@gmail.com>
2021-10-23 00:35:11 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Bitcoin Price Index</h1>
+    <section v-if="errored">
+      <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
+    </section>
+    <section v-else>
+      <div v-if="loading">Loading...</div>
+      <div
+        v-else
+        v-for="currency in info"
+        v-bind:key="currency.code"
+        class="currency"
+        >
+        {{ currency.description }}:
+        <span class="lighten">
+          <span v-html="currency.symbol"></span>{{ currencyDecimal(currency.rate_float) }}
+        </span>
+      </div>
+    </section>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  el:'#app',
+  data () {
+    return {
+      info: null,
+      loading: true,
+      errored: false
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response.data.bpi))
+        .catch(error => {
+          console.log(error)
+          this.errored = true
+          })
+        .finally(() => this.loading = false)
+  },
+  methods: {
+    currencyDecimal (value) {
+      return value.toFixed(2)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
